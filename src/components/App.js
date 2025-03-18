af042cc6e85a6a13f97e931fe07531ecf961dd4e
Krishna Mehta <131295594+KDMEHTA704@users.noreply.github.com>
2025-03-18 14:33:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   const [name, setName] = useState(""); 
+
   return (
     <div>
         <p>Enter your name:</p>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)}  />
-        <p>Hello {name}</p>
+        <input 
+            type="text" 
+            value={name} 
+            onChange={(e) => setName(e.target.value)}
+            data-testid="name-input"
+        />
+        <p data-testid="greeting">Hello {name}!</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
